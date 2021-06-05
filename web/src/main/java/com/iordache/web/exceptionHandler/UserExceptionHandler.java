eb24b635ba38814d8e13c170b0ca1aefb17fbd62
iordache <faraonu_ginitoru@yahoo.com>
2021-06-05 18:38:20 +0300

    created UserExceptionHandler.class to handle the generated exceptions in the program

--- web/src/main/java/com/iordache/web/exceptionHandler/UserExceptionHandler.java
+++ web/src/main/java/com/iordache/web/exceptionHandler/UserExceptionHandler.java
@@ -1,12 +1,37 @@
 package com.iordache.web.exceptionHandler;
 
+import com.iordache.exceptions.errors.UserAlreadyExists;
+import com.iordache.exceptions.errors.UserNotFoundException;
+import com.iordache.exceptions.message.UserErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class UserExceptionHandler {
 
 
+    @ExceptionHandler  //method 2
+    public ResponseEntity<UserErrorMessage> handleUserAlreadyExistsException(UserAlreadyExists exc){
 
+        var error = userErrorMessage(HttpStatus.FOUND.value(), exc.getMessage(), exc);
+        return new ResponseEntity<>(error, HttpStatus.FOUND);
+    }
+
+    @ExceptionHandler  //method 2
+    public ResponseEntity<UserErrorMessage> handleUserNotFoundException(UserNotFoundException exc){
+
+        var error = userErrorMessage(HttpStatus.NOT_FOUND.value(), exc.getMessage(), exc);
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+                       //method 1
+    private UserErrorMessage userErrorMessage(int status, String message, RuntimeException exc){
+        exc.printStackTrace();
+        return new UserErrorMessage(status, message, System.currentTimeMillis());
+
+    }
 
 
 
